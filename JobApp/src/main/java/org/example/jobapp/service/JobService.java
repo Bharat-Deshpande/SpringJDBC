90ba22e4f1990ef765fa202a79dd3f659bfe9a75
Bharat Deshpande <149424252+Bharat-Deshpande@users.noreply.github.com>
2025-02-27 15:05:34 +0530

    Add files via upload

---
+++ JobApp/src/main/java/org/example/jobapp/service/JobService.java
@@ -0,0 +1,24 @@
+package org.example.jobapp.service;
+
+import org.example.jobapp.model.JobPost;
+import org.example.jobapp.repo.JobRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.w3c.dom.stylesheets.LinkStyle;
+
+import java.util.List;
+
+@Service
+public class JobService {
+
+    @Autowired
+    private JobRepo repo;
+
+    public void addJob(JobPost jobPost){
+        repo.addJob(jobPost);
+    }
+
+    public List<JobPost> getAllJobs(){
+        return repo.getAllJobs();
+    }
+}
